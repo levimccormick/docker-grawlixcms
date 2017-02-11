97cf714e13aee34ce380cacd52524552861d4047
Levi McCormick <lwmccormick@spscommerce.com>
2017-02-11 13:07:18 -0600

    Initial commit.

---
+++ grawlixcms/config.php
@@ -0,0 +1,6 @@
+<?php
+// This is purely a local config file. Do not upload this to your live site.
+$setup['db_host'] = $_ENV['DB_SERVER'];
+$setup['db_user'] = $_ENV['DB_USER'];
+$setup['db_pswd'] = $_ENV['DB_PASS'];
+$setup['db_name'] = $_ENV['DB_NAME'];
