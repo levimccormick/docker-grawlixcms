cbda6e5050e459a6f3ef6512c7a2ccb3f1b935dd
Levi McCormick <lwmccormick@spscommerce.com>
2017-02-11 13:41:42 -0600

    Local env aware config file.

--- grawlixcms/config.php
+++ grawlixcms/config.php
@@ -1,6 +1,14 @@
 <?php
-// This is purely a local config file. Do not upload this to your live site.
-$setup['db_host'] = $_ENV['DB_SERVER'];
-$setup['db_user'] = $_ENV['DB_USER'];
-$setup['db_pswd'] = $_ENV['DB_PASS'];
-$setup['db_name'] = $_ENV['DB_NAME'];
+if($_ENV['LOCAL'] == 'True'){
+  // These are local values for running in Docker.
+  $setup['db_host'] = $_ENV['DB_SERVER'];
+  $setup['db_user'] = $_ENV['DB_USER'];
+  $setup['db_pswd'] = $_ENV['DB_PASS'];
+  $setup['db_name'] = $_ENV['DB_NAME'];
+}else{
+  // Put your live site values here.
+  $setup['db_host'] = '';
+  $setup['db_user'] = '';
+  $setup['db_pswd'] = '';
+  $setup['db_name'] = '';
+}
